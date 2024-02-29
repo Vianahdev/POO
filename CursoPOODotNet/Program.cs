420c254a06d45ade736c06e5eebb2e293423d2b1
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2024-02-29 08:17:38 -0300

    Feat: Encapsulamento

---
+++ CursoPOODotNet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CursoPOODotNet{
+    class Program{
+        static void Main(string[] args){
+
+        }
+    }
+
+    class Payments{
+        //Propriedades
+        DateTime Vencimento;
+        string pessoa_Name;
+        string pessoa_CPF;
+        string pessoa_Telefone;
+
+        //Métodos
+        void Pagar(){ }
+    }
+}
